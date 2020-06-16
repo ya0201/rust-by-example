44125ed6fb3c9deb551d183269d7085c2c1688ce
ya0201 <hoge@example.com>
2020-06-16 18:18:09 +0900

    Initial commit

---
+++ print/src/main.rs
@@ -0,0 +1,39 @@
+use std::fmt;
+
+fn main() {
+    println!("Hello, world!");
+    println!("{} days", 31);
+
+    println!("{0}, this is {1}. {1}, this is {0}.", "Alice", "Bob");
+
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+
+    println!(
+        "{} of {:b} people know binary, the other half doesn't",
+        1, 2
+    );
+
+    println!("{number:>width$}", number = 1, width = 6);
+
+    println!("{number:>0width$}", number = 1, width = 6);
+
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    // #[allow(dead_code)]
+    // tuple struct
+    // see: https://doc.rust-lang.org/book/ch05-01-defining-structs.html#using-tuple-structs-without-named-fields-to-create-different-types
+    struct Structure(i32);
+
+    impl fmt::Display for Structure {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "({})", self.0)
+        }
+    }
+
+    println!("Struct: {} ", Structure(3));
+}
